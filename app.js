326453fcad937384102b6ec2e431c3106232dbb8
Prasad More <prasadmoremain@gmail.com>
2024-01-23 17:20:12 +0530

    Added Card

--- app.js
+++ app.js
@@ -19,8 +19,41 @@ employees.onload = function(){
         }
     
     }
-    console.log(empArr)
+    display()
 
     
+}
+
+
+
+function display(){
+    console.log(empArr)
+    let card = document.getElementById('card')
+    let fname = document.getElementById('fname')
+    let email = document.getElementById('email')
+    let bday = document.getElementById('bday')
+    let gender = document.getElementById('gender')
+    let pno = document.getElementById('pno')
+
+    for(let i=0;i<empArr.length;i++){
+        let cards = document.createElement('div')
+        fname.innerText = empArr[i].firstName
+        email.innerText = empArr[i].email
+        bday.innerText = empArr[i].birthDate
+        gender.innerText = empArr[i].gender
+        pno.innerText = empArr[i].phone
+
+
+        cards.appendChild()
+        cards.appendChild(email.innerText)
+        cards.appendChild(bday.innerText)
+        cards.appendChild(gender.innerText)
+        cards.appendChild(pno.innerText)
+        card.appendChild(cards)
+        
+
+        console.log(cards)
+    }
+
 }
     
